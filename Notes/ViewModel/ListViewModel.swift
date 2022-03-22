aee2c8ddc050838e454a111bc527fcf939400b5d
rleymi <archerlv99@gmail.com>
2022-03-22 18:06:54 +0600

    User Defaults data set

--- Notes/ViewModel/ListViewModel.swift
+++ Notes/ViewModel/ListViewModel.swift
@@ -8,26 +8,40 @@
 import Foundation
 
 /*
+ ViewModel
+ 
  Create
  Read
  Update
  Delete
  */
+
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "items_ list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "First", isMarked: false),
-            ItemModel(title: "Second", isMarked: true),
-            ItemModel(title: "Third", isMarked: false)
-        ]
-        items.append(contentsOf: newItems)
+//        let newItems = [
+//            ItemModel(title: "First", isMarked: false),
+//            ItemModel(title: "Second", isMarked: true),
+//            ItemModel(title: "Third", isMarked: false)
+//        ]
+//        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -43,6 +57,11 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }

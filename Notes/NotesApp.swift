f26f989e29cf3207848425d6bd70c28c88b75edc
rleymi <archerlv99@gmail.com>
2022-03-22 15:46:09 +0600

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Arthur Lee on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
